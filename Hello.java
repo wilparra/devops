795c8d23ed24f449a8285d8175ba6317a67b2323
William <wilparralba@gmail.com>
2019-05-01 22:30:31 -0500

    Se adiciona contador

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main (String [] args){
 		for(int i=1; i<=10;i++){
+			System.out.println("-------"+i+"-----------");
 			System.out.println("------------------");
 			System.out.println("Hola mundo ... "+i);
 			System.out.println("------------------");
