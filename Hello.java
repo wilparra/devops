0a6abb20ea72de38a64770c2918c2bf4503f27af
William <wilparralba@gmail.com>
2019-05-01 22:02:28 -0500

    Nuevos comentarios agregados

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 public class Hello{
 	public static void main (String [] args){
 		for(int i=1; i<=10;i++){
+			System.out.println("------------------");
 			System.out.println("Hola mundo ... "+i);
 			System.out.println("------------------");
+			
 		}
 	}
 	
