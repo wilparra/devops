9d0c32b21b3dfa1248d3fbad321067cfc9bd1af7
William Enrique Parra <wilparralba@gmail.com>
2019-05-01 22:27:49 -0500

    Update Hello.java
    Se adiciona comentario

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 	public static void main (String [] args){
 		for(int i=1; i<=10;i++){
 			System.out.println("------------------");
+			System.out.println("------------------");
 			System.out.println("Hola mundo ... "+i);
 			System.out.println("------------------");
 			
